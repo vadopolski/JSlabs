0884bcd9fd21fbb8d1c5845d4cb04069e43e9fdc
vadopolski <vop444@gmail.com>
2018-04-10 09:29:58 +0300

    Added multiselect and submit button.

--- Scripts/module3lab6.js
+++ Scripts/module3lab6.js
@@ -1,11 +1,9 @@
-alert('test');
-
 function validate(id, regExp) {
     var parent = document.getElementById(id);
     var inputValue = document.getElementById(id + "Input").value;
-    var childAfter = document.getElementById(id + "After");
+    var validationResultElement = document.getElementById(id + "After");
 
-    if (childAfter != null) {
+    if (validationResultElement != null) {
         updateFilledElement(parent.lastChild, checkValue(inputValue, regExp));
     } else {
         var newItem = document.createElement("span");
@@ -22,13 +20,21 @@ function updateFilledElement(child, st) {
     } else {
         child.innerHTML = "&nbsp;&nbsp;&nbsp;Correct";
         child.setAttribute("style", "color: chartreuse");
-        if (++count < dataJSON.length && document.getElementById(dataJSON[count].id) == null) {
-            mainForm.appendChild(document.createElement("br"));
-            mainForm.appendChild(document.createElement("br"));
-            mainForm.appendChild(generateInput(dataJSON[count].id, dataJSON[count].type));
-        }
+        addNextSpanSectionToMainForm();
     }
     return child;
+}
+
+function addNextSpanSectionToMainForm() {
+    if (++count < dataJSON.length && document.getElementById(dataJSON[count].id) == null) {
+        mainForm.appendChild(document.createElement("br"));
+        mainForm.appendChild(document.createElement("br"));
+        mainForm.appendChild(generateInput(dataJSON[count].id, dataJSON[count].type));
+    } else {
+        mainForm.appendChild(document.createElement("br"));
+        mainForm.appendChild(document.createElement("br"));
+        mainForm.appendChild(generateSubmit());
+    }
 }
 
 function checkValue(st, regExp) {
@@ -47,16 +53,46 @@ function generateInput(idName, type) {
     var input = document.createElement("input");
     input.setAttribute("id", idName + "Input");
     input.setAttribute("type", type);
+    if(type == "text" && input.getAttribute("id") == dataJSON[0].id + "Input"){
+        input.setAttribute("list", "myData");
+    }
     input.setAttribute("onblur", "validate(dataJSON[count].id, dataJSON[count].spanPattern)");
     parent.appendChild(label);
     parent.appendChild(input);
     return parent;
 }
-var dataJSON = [{"id": "username", "spanPattern": "^[A-Z]+[a-z]{0,10}$", "type": "text"},
-    {"id": "age", "spanPattern": "^[0-9]{3,9}$", "type": "number"},
-    {"id": "email", "spanPattern": "^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$", "type": "text"}];
+
+function generateDataList() {
+    var dataList = document.createElement("datalist");
+    dataList.setAttribute("id", "myData");
+    for(var i = 0; i < dataListJSON.length; i++){
+        var option = document.createElement("option");
+        option.setAttribute("label", dataListJSON[i].label);
+        option.setAttribute("value", dataListJSON[i].value);
+        dataList.appendChild(option);
+    }
+    return dataList;
+}
+
+function generateSubmit() {
+    var submit = document.createElement("input");
+    submit.setAttribute("type", "button");
+    submit.setAttribute("value", "Submit");
+    submit.setAttribute("onclick", "deleteInput()");
+    return submit;
+}
+
+function deleteInput() {
+    mainForm.delet
+}
+
+var dataListJSON = [{label: "(Суперпользователь)", value: "Admin"},
+                    {label: "(Посетитель)", value: "User"}, {label: "(Кот. Просто кот)", value: "Cat"}];
+
+var dataJSON = [{id: "username", spanPattern: "^[A-Z]+[a-z]{0,10}$", type: "text"},
+    {id: "age", spanPattern: "^[0-9]{3,9}$", type: "number"},
+    {id: "email", spanPattern: "^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$", type: "text"}];
 var count = 0;
 var currentID = dataJSON[count].id;
 var currentSpanPattern = dataJSON[count].spanPattern;
 var currentType = dataJSON[count].type;
-var mainForm = document.getElementById("mainForm");
